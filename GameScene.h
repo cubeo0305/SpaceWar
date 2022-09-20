0d981c3fd097c7f0c16f6ff09c26176b95c70564
cubeo0305 <mcuong0305@gmail.com>
2022-09-20 12:54:39 +0700

    SpaceWar

---
+++ GameScene.h
@@ -0,0 +1,22 @@
+#ifndef __GAMESCENE_SCENE_H__
+#define __GAMESCENE_SCENE_H__
+
+#include "cocos2d.h"
+#include "Player.h"
+
+class GameScene : public cocos2d::Scene
+{
+public:
+    static cocos2d::Scene* createScene();
+    virtual bool init();
+        
+    Player* player;
+ 
+    // a selector callback
+    void menuCloseCallback(cocos2d::Ref* pSender);
+    
+    // implement the "static create()" method manually
+    CREATE_FUNC(GameScene);
+};
+
+#endif // __HELLOWORLD_SCENE_H__
--- Player.cpp
+++ Player.cpp
@@ -1,61 +1,25 @@
-/****************************************************************************
- Copyright (c) 2017-2018 Xiamen Yaji Software Co., Ltd.
- 
- http://www.cocos2d-x.org
- 
- Permission is hereby granted, free of charge, to any person obtaining a copy
- of this software and associated documentation files (the "Software"), to deal
- in the Software without restriction, including without limitation the rights
- to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- copies of the Software, and to permit persons to whom the Software is
- furnished to do so, subject to the following conditions:
- 
- The above copyright notice and this permission notice shall be included in
- all copies or substantial portions of the Software.
- 
- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- THE SOFTWARE.
- ****************************************************************************/
-
 #include "Player.h"
 
 USING_NS_CC;
 
-Scene* Player::createScene()
+Player* Player::createPlayer()
 {
     return Player::create();
 }
 
-// Print useful error message instead of segfaulting when files are not there.
-static void problemLoading(const char* filename)
-{
-    printf("Error while loading: %s\n", filename);
-    printf("Depending on how you compiled you might have to add 'Resources/' in front of filenames in HelloWorldScene.cpp\n");
-}
-
-// on "init" you need to initialize your instance
 bool Player::init()
 {
     //////////////////////////////
     // 1. super init first
-    if ( !Scene::init() )
+    if ( !Node::init() )
     {
         return false;
     }
-
-    auto visibleSize = Director::getInstance()->getVisibleSize();
-    Vec2 origin = Director::getInstance()->getVisibleOrigin();
-
     this->speed = 200;
     this->direction = Vec2(1, 1);
 
     this->player = Sprite::create("Player.png");
-    this->player->setPosition(Vec2(visibleSize.width / 2, visibleSize.height / 2));
+    this->player->setPosition(Vec2(300, 300));
     addChild(this->player);
 
     //init Mouse
@@ -99,7 +63,7 @@ void Player::onMouseDown(Event* event) {
 void Player::Shooting() {
     // create bullet at sprite position and aim to targetPos
     auto bullet = Sprite::create("Bullet.png");
-    bullet->setContentSize(Size(30, 40));
+    bullet->setContentSize(Size(15, 20));
     bullet->setPosition(this->player->getPosition());
     addChild(bullet);
 
@@ -111,7 +75,6 @@ void Player::Shooting() {
     bullet->runAction(sequence);
 
 }
-
 void Player::setIsShooting(bool isShooting)
 {
     if (isShooting) {
